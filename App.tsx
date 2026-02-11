d790f75f119296faaa41bef0390528f00e6abc97
CoreBlock <bpzx7077@gmail.com>
2026-02-11 08:00:52 +0700

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { SignalList } from "./components/SignalList";
+
+export default function App() {
+  return (
+    <div>
+      <h1>COREBLOCK Signals</h1>
+      <SignalList />
+    </div>
+  );
+}
